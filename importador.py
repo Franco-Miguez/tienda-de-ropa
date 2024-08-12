dbb9019171b62d47c3d6baa3f37cd74e6b68b07b
Franco <franco.miguez98@gmail.com>
2024-08-12 15:45:06 -0300

    Clase 12/8

--- importador.py
+++ importador.py
@@ -22,6 +22,49 @@ class Importador():
                     lista.append( Accesorio(codigo, material, precio, stock, descripcion) )
             return lista
             
+    @classmethod
+    def exportar(
+                    cls, nombre : str, stock : int, precio : float,
+                    genero : str | None = False, talle : str | None = False, material : str | None = False
+                ):
+        articulo = {}
+        articulo["Codigo"] = cls.generar_codigo()
+        articulo["Descripcion"] = nombre
+        articulo["Stock"] = stock
+        articulo["Precio"] = precio
+        if talle: 
+            ruta = "./csv/ropa.csv"
+            articulo["Genero"] = genero
+            articulo["Talle"] = talle
+        else:
+            ruta = "./csv/accesorios.csv"
+            articulo["Material"] = material
+        with open(ruta, mode="a") as archivo:
+            titulos = ["Codigo","Precio","Stock","Descripcion"]
+            if talle:
+                titulos.insert(1,"Genero") 
+                titulos.insert(1,"Talle") 
+            else:
+                titulos.insert(1,"Material")
+                
+            escribir = csv.DictWriter(archivo, titulos)
+            
+            escribir.writerow(articulo)
+    
+    @classmethod
+    def generar_codigo(cls):
+        with open("./csv/accesorios.csv") as archivo:
+            info = csv.DictReader(archivo)
+            for linea in info:
+                codigo_accesorios = linea.get("Codigo")
+        with open("./csv/ropa.csv") as archivo:
+            info = csv.DictReader(archivo)
+            for linea in info:
+                codigo_ropa = linea.get("Codigo")
+        if codigo_ropa > codigo_accesorios:
+            return str(int(codigo_ropa) + 1)
+        else:
+            return str(int(codigo_accesorios) + 1)
 
 if __name__ == "__main__":
     productos = Importador.importar("ropa.csv")
--- menu.py
+++ menu.py
@@ -1,4 +1,6 @@
 from importador import Importador
+from productos.ropa import Ropa
+from productos.accesorio import Accesorio
 
 class Menu():
     def __init__(self) -> None:
@@ -8,22 +10,57 @@ class Menu():
     def mostrar_menu(self):
         while True:
             print("\n\n\tMenu:")
-            opcion = input("1. Vender\n2. Mostrar Stock\n3. Agregar Productos\n4. Salir\nOpcion: ")
+            opcion = input("1. Vender\n2. Mostrar Stock\n3. Agregar stock\n4. Salir\nOpcion: ")
             if opcion == "1":
                 pass
             elif opcion == "2":
-                for articulo in self.stock:
-                    print(articulo.get_info())
-                    
+                self.mostrar_stock()
             elif opcion == "3":
-                pass
+                self.agregar_stock()
             elif opcion == "4":
                 quit()
             else:
                 print("#"*10,"\n\tError la opcion que selecciono no esta disponible\n","#"*10, sep="")
     
-    def mostrar_stock():
-        pass
+    def mostrar_stock(self):
+        for articulo in self.stock:
+            info = articulo.get_info()
+            print(f"\n\t{info.get('Descripcion')}")
+            print(f"Codigo: {info.get('Codigo')}")
+            print(f"Stock: {info.get('Stock')}")
+            print(f"Precio: {info.get('Precio')}")
+            print(type(articulo) == type(Ropa("001","Xl", "Masculino")))
+            if type(articulo) == type(Ropa("001","Xl", "Masculino")):
+                print(f"Genero: {info.get("Genero")}")
+                print(f"Talle: {info.get("Talle")}")
+            else:
+                print(f"Material: {info.get("Material")}")
+    
+    def agregar_stock(self):
+        tipo = input("Ingrese que quiere agregar:\n1. Ropa\n2. Accesorio\nEleccion: ")
+        nombre = input("Nombre: ")
+        stock = input("Stock: ")
+        precio = input("Precio: ")
+        if tipo == "1":
+            
+            genero = input("1. Masculino\n2. Femenino\n3. unisex\nGenero: ")
+            talle = input ("1. S\n2. M\n3. L\n4. XL\n Talles: ")
+            
+            if genero == "1": genero = "Masculino"
+            elif genero == "2": genero = "Femenio"
+            else: genero = "Unisex"
+                
+            if talle == "1": talle = "S"
+            elif talle == "2": talle = "M"
+            elif talle == "3": talle = "L"
+            elif talle == "4": talle = "XL"
+            
+            Importador.exportar(nombre,stock,precio,genero,talle)
+        else:
+            material = input("Material: ")
+            Importador.exportar(nombre, stock, precio, material=material)
+        
+
 
 if __name__== "__main__":
     menu = Menu()
